155fe2a118b37a768c90aeab6563266298f34db8
Dripskl <joaovitorlacerdapm@gmail.com>
2025-12-15 16:18:21 -0300

    Add filtering options to blog posts and style form elements

--- 5blog/blog.php
+++ 5blog/blog.php
@@ -2,9 +2,8 @@
 include '../conecta_mysql.inc';
 include '../6admin/protect.php';
 
-// Se enviou o formulário
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+// --- ADICIONAR POST (form de add.html envia para cá) ---
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nome"], $_POST["descricao"], $_POST["nota"])) {
     $nome = $_POST["nome"];
     $descricao = $_POST["descricao"];
     $nota = $_POST["nota"];
@@ -19,8 +18,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-$sql = "SELECT * FROM blog ORDER BY id DESC";
+// --- FILTRO ---
+$ordem = $_POST['ordem'] ?? 'padrao';
+
+$sql = "SELECT * FROM blog";
+
+if ($ordem == "decrescente") {
+    $sql .= " ORDER BY nota DESC";
+} elseif ($ordem == "crescente") {
+    $sql .= " ORDER BY nota ASC";
+} elseif ($ordem == "recente") {
+    $sql .= " ORDER BY id DESC";
+}
+
 $result = mysqli_query($conexao, $sql);
+
+if (!$result) {
+    die("Erro na query: " . mysqli_error($conexao));
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -32,54 +47,58 @@ $result = mysqli_query($conexao, $sql);
 </head>
 <body>
 
-<!-- Cabeçalho -->
 <header>
     <a href="../index.php"><img src="../imagens/LELEYFLIX - logo.png" alt="Logo"></a>
     <ul>
-         <li><a href="../index.php">Início</a></li>
-            <li><a href="../2series/series.php">Séries</a></li>
-            <li><a href="../3filmes/filmes.php">Filmes</a></li>
-            <li><a href="../4formulario/formulario.php">Indique uma Série ou Filme</a></li>
-            <li><a href="../5blog/blog.php">Blog</a></li>
-            <li><a href="../6admin/logar.php">login</a></li>
-            <?php if (isset($_SESSION['id']) && $_SESSION['id'] == 1): ?>
-            <li><a href="../6admin/admin.php">Admin</a></li>
-            <?php endif; ?>
-
+        <li><a href="../index.php">Início</a></li>
+        <li><a href="../2series/series.php">Séries</a></li>
+        <li><a href="../3filmes/filmes.php">Filmes</a></li>
+        <li><a href="../4formulario/formulario.php">Indique uma Série ou Filme</a></li>
+        <li><a href="../5blog/blog.php">Blog</a></li>
+        <li><a href="../6admin/logar.php">login</a></li>
+        <?php if (isset($_SESSION['id']) && $_SESSION['id'] == 1): ?>
+        <li><a href="../6admin/admin.php">Admin</a></li>
+        <?php endif; ?>
     </ul>
 </header>
 
 <br>
+<!-- FILTRO DE ORDEM -->
+<form method="post">
+    <select name="ordem">
+        <option value="padrao" <?= $ordem == 'padrao' ? 'selected' : '' ?>>Padrão</option>
+        <option value="decrescente" <?= $ordem == 'decrescente' ? 'selected' : '' ?>>maior pra menor nota</option>
+        <option value="crescente" <?= $ordem == 'crescente' ? 'selected' : '' ?>>menor pra maior nota</option>
+        <option value="recente" <?= $ordem == 'recente' ? 'selected' : '' ?>>Recente</option>
+    </select>
+    <input type="submit" value="Filtrar">
+</form>
 
-<!-- Lista dos cards -->
+<!-- Botão adicionar / logar -->
 <div class="botao">
-    <?php if(isset($_SESSION)):?>
-    <a href="add.html" class="btn">Adicionar</a>
+    <?php if(isset($_SESSION)): ?>
+        <a href="add.html" class="btn">Adicionar</a>
+    <?php else: ?>
+        <a href="../6admin/logar.php" class="btn">Logar</a>
     <?php endif; ?>
-    <?php if(!isset($_SESSION)):?>
-    <a href="../6admin/logar.php" class="btn">Logar</a>
-    <?php endif; ?>
-    
 </div>
 
-
+<!-- LISTA DE POSTS -->
 <?php while ($c = mysqli_fetch_assoc($result)) : ?>
     <div class="cards">
         <div class="card">
             <div class="card-text">
                 <?php if (isset($_SESSION['id']) && $_SESSION['id'] == 1): ?>
-                <h2><?= $c["id"] ?></h2>
+                    <h2><?= $c["id"] ?></h2>
                 <?php endif; ?>
                 <h2 class="Name"><?= $c["nome"] ?></h2>
                 <h4><?= $c["descricao"] ?></h4>
                 <h4>Nota: &#9733; <?= number_format($c["nota"], 2, ',', '.') ?></h4><br><br>
-                
             </div>
         </div>
     </div>
     <br>
 <?php endwhile; ?>
-
 
 </body>
 </html>
